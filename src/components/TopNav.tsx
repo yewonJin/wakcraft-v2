e06f6d7722ddecb68f699a5d677ce1cca08f2127
yewonJin <jyw966@naver.com>
2023-11-07 20:31:24 +0900

    [UPDATE] TopNav 아래로 스크롤 시 숨기기 기능

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,19 +5,35 @@ import DarkMode from "../../public/dark_mode.svg";
 import useTheme from "@/hooks/useTheme";
 
 export default function TopNav() {
-  const { handleModeClick } = useTheme();
+  const { scrollY, isScrolled, handleModeClick } = useTheme();
 
   return (
-    <nav className="mx-auto flex h-20 max-w-[1200px] items-center justify-between">
-      <h1 className=" text-2xl font-semibold text-text-primary">WAKCRAFT</h1>
-      <div className="flex items-center">
-        <span
-          className="flex fill-text-secondary hover:cursor-pointer hover:fill-text-primary [&>*:first-child]:dark:hidden [&>*:last-child]:hidden [&>*:last-child]:dark:block"
-          onClick={handleModeClick}
+    <nav
+      className={`fixed h-20 w-full ${
+        !isScrolled && scrollY >= 100 ? "invisible" : "visible"
+      } ${scrollY <= 100 ? "bg-none" : "bg-backround-primary"}`}
+    >
+      <div className="mx-auto flex h-full w-[1200px] items-center justify-between">
+        <h1
+          className={`text-2xl font-semibold ${
+            scrollY <= 100 ? "text-[white]" : "text-text-primary"
+          }`}
         >
-          <LightMode />
-          <DarkMode />
-        </span>
+          WAKCRAFT
+        </h1>
+        <div className="flex items-center">
+          <span
+            className={`flex ${
+              scrollY <= 100
+                ? "fill-[#ccc] hover:fill-[white]"
+                : "fill-text-primary hover:fill-text-primary"
+            } hover:cursor-pointer [&>*:first-child]:dark:hidden [&>*:last-child]:hidden [&>*:last-child]:dark:block`}
+            onClick={handleModeClick}
+          >
+            <LightMode />
+            <DarkMode />
+          </span>
+        </div>
       </div>
     </nav>
   );
