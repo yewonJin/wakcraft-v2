2164452a4ccb90e6222ad9f133c81beefae46040
yewonJin <jyw966@naver.com>
2023-11-08 21:22:59 +0900

    [UPDATE] cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import ArchitectureInfo from "@/components/common/ArchitectureInfo";
-import ImageBox from "@/components/common/ImageBox";
-
 import Home from "../components/Home/Home";
 
 export default function Page() {
--- src/components/Home/BackgroundImage.tsx
+++ src/components/Home/BackgroundImage.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundImage() {
   return (
     <div
-      className="absolute left-0 top-0 z-[-1] flex h-full w-full items-center justify-center bg-backround-secondary bg-main bg-cover bg-center bg-no-repeat"
+      className="bg-background-secondary absolute left-0 top-0 z-[-1] flex h-full w-full items-center justify-center bg-main bg-cover bg-center bg-no-repeat"
       style={{
         backgroundBlendMode: "darken",
         backgroundColor: "rgba(0,0,0, 0.6)",
