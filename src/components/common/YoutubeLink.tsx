e5e9a046085790b5bc99345953d0c0de2ea1f67a
yewonJin <jyw966@naver.com>
2023-11-10 03:02:13 +0900

    [FIX] 유튜브 링크 클릭할 때 이미지 창 같이 열리는 문제 해결

--- src/components/common/YoutubeLink.tsx
+++ src/components/common/YoutubeLink.tsx
@@ -13,8 +13,11 @@ export default function YoutubeLink(props: Props) {
 
   return (
     <div
-      className="group/youtube peer absolute right-2 top-2 z-10 w-8 rounded-lg bg-[#121212] fill-[#fff] p-[2px] text-left opacity-90 hover:cursor-pointer hover:rounded-l-none hover:opacity-100"
-      onClick={() => window.open(youtubeUrl)}
+      className="group/youtube peer absolute right-2 top-2 z-10 w-8 rounded-lg bg-[#121212] fill-[#fff] p-[3px] text-left opacity-90 hover:cursor-pointer hover:rounded-l-none hover:opacity-100 [&>svg]:rotate-[135deg]"
+      onClick={(e) => {
+        e.stopPropagation();
+        window.open(youtubeUrl);
+      }}
     >
       <Link />
       <p className="invisible absolute right-8 top-0 flex h-8 w-max items-center bg-[#121212] pb-[1px] pl-[10px] pr-[6px] text-sm text-[white] group-hover/youtube:visible group-hover/youtube:rounded-l-lg">
