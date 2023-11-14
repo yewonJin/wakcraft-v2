c47b42fb7e09178a0af8f5322a49ad40418ed292
yewonJin <jyw966@naver.com>
2023-11-14 20:57:30 +0900

    [UPDATE] 배치고사 페이지

---
+++ src/components/PlacementTest/ArchitectureInfo.tsx
@@ -0,0 +1,26 @@
+import { Tier } from "@/domains/architect";
+import TierBox from "../common/TierBox";
+import { Fragment } from "react";
+
+type Props = {
+  placementResult: Tier;
+  minecraft_id: string;
+};
+
+export default function ArchitectureInfo(props: Props) {
+  const { placementResult, minecraft_id } = props;
+
+  return (
+    <Fragment>
+      <div className="absolute left-2 top-2 flex flex-col gap-16 md:left-3 md:top-3 [&>span]:text-base">
+        <TierBox tier={placementResult} width="65px" height="75px" />
+      </div>
+      <p
+        className="absolute bottom-4 left-0 bg-[rgba(0,0,0,0.6)] py-1 pl-3 pr-2 text-base text-[white]"
+        style={{ textShadow: "1px 1px 5px #222" }}
+      >
+        {minecraft_id}
+      </p>
+    </Fragment>
+  );
+}
