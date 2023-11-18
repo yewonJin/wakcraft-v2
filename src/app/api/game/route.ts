e45aefcc875548066019c323c457b2c5cf96c668
yewonJin <jyw966@naver.com>
2023-11-18 16:41:10 +0900

    [UPDATE] 게임 문제 만드는 api route

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -1,17 +1,71 @@
-import NoobProHacker from "@/models/noobprohacker";
-import connectMongo from "@/utils/connectMongo";
 import { NextRequest, NextResponse } from "next/server";
+
+import Architect from "@/models/architect";
+import connectMongo from "@/utils/connectMongo";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
+    const { searchParams } = new URL(req.url);
+
+    const params = searchParams.get("difficulty");
+
+    if (!params) {
+      return NextResponse.json(JSON.stringify({ message: "error" }), {
+        status: 400,
+      });
+    }
+
     connectMongo();
 
-    const result = await NoobProHacker.findAll();
+    if (params === "LOW") {
+      const res = [
+        ...(await Architect.findByTier("hacker")),
+        ...(await Architect.findByTier("gukbap")),
+      ];
 
-    return NextResponse.json(result, { status: 200 });
+      return NextResponse.json(convertToGameObject(res), { status: 200 });
+    }
+
+    if (params === "MEDIUM") {
+      const res = [
+        ...(await Architect.findByTier("gukbap")),
+        ...(await Architect.findByTier("pro")),
+        ...(await Architect.findByTier("gyeruik")),
+      ];
+
+      return NextResponse.json(convertToGameObject(res), { status: 200 });
+    }
+
+    return NextResponse.json("hi", { status: 200 });
   } catch (e) {
     return NextResponse.json(JSON.stringify({ message: "error" }), {
       status: 400,
     });
   }
 }
+
+const convertToGameObject = (arr: Architect[]) => {
+  return arr
+    .map((architect) =>
+      [
+        ...architect.portfolio.noobProHacker.map((item) => ({
+          image_url: item.image_url,
+          minecraft_id: architect.minecraft_id,
+        })),
+        ,
+        ...architect.portfolio.eventNoobProHacker
+          .filter((item) => item.episode !== 5)
+          .filter((item) => item.episode !== 3)
+          .map((item) => ({
+            image_url: item.image_url,
+            minecraft_id: architect.minecraft_id,
+          })),
+        ,
+        ...architect.portfolio.architectureContest.map((item) => ({
+          image_url: item.image_url,
+          minecraft_id: architect.minecraft_id,
+        })),
+      ].filter((item) => item),
+    )
+    .flat();
+};
