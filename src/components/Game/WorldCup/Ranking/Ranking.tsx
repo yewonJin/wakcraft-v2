81442464631a06e41aa86e0c181748cddb7f5faa
yewonJin - Mac <>
2023-12-01 00:41:49 +0900

    [STYLE] 반응형 스타일

--- src/components/Game/WorldCup/Ranking/Ranking.tsx
+++ src/components/Game/WorldCup/Ranking/Ranking.tsx
@@ -3,8 +3,12 @@ import ArchitectureList from "./ArchitectureList";
 
 export default function Ranking() {
   return (
-    <div className="mx-auto max-w-[1200px] px-2 pb-8 pt-24 sm:px-4 xl:px-0 xl:pt-32">
-      <h1 className={`text-3xl text-text-primary ${medium.className}`}>랭킹</h1>
+    <div className="mx-auto max-w-[1200px] px-2 pb-8 pt-24 xl:px-0 xl:pt-32">
+      <h1
+        className={`px-2 text-3xl text-text-primary xl:px-0 ${medium.className}`}
+      >
+        랭킹
+      </h1>
       <div className="mt-8 flex h-[70px] w-full items-center gap-4 rounded-md bg-background-secondary px-3 text-sm text-text-secondary sm:gap-10 sm:px-6 sm:text-base">
         <p className="w-[30px] text-center">순위</p>
         <p className="w-[90px] sm:w-[150px]">이미지</p>
