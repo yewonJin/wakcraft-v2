9612eaa0dd89aafcbfe08f896e33b6fe6047f0eb
yewonJin <jyw966@naver.com>
2023-11-30 15:43:27 +0900

    [UPDATE] 게임 만드는 중일 때 loading 표시해주기

--- src/hooks/Game/WhoseWork/useSetting.ts
+++ src/hooks/Game/WhoseWork/useSetting.ts
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useRecoilValue } from "recoil";
+import { useQuery } from "react-query";
+import toast from "react-hot-toast";
 
 import { shuffle } from "@/utils/lib";
 import { WhoseWork } from "@/domains/whoseWork";
@@ -28,12 +30,23 @@ const useSetting = () => {
   const [questions, setQuestions] = useState<Question[]>([]);
   const [whoseWork, setWhoseWork] = useState<WhoseWork>();
 
+  const { refetch } = useQuery(
+    ["getWhoseWorkArchitectures"],
+    () => getArchitectures(difficulty),
+    {
+      enabled: false,
+    },
+  );
+
   const startGame = async () => {
     if (difficulty === null || numberOfArchitecture === null) return;
 
-    const architectures: Question[] = await getArchitectures(difficulty);
+    toast.loading("게임을 만드는 중입니다...", { id: "whoseWork_loading" });
 
-    setQuestions(shuffle(architectures).slice(0, numberOfArchitecture));
+    await refetch().then((res) => {
+      setQuestions(shuffle(res.data).slice(0, numberOfArchitecture));
+      toast.remove("whoseWork_loading");
+    });
 
     setPage((prev) => prev + 1);
   };
