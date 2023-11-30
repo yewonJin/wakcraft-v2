771f2c31948829dd7ebf68650e0485f7e29493bd
yewonJin <jyw966@naver.com>
2023-11-30 20:49:22 +0900

    [UPDATE] 모바일 환경에서 TopNav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,10 +5,15 @@ import { usePathname } from "next/navigation";
 
 import LightMode from "../../public/icons/light_mode.svg";
 import DarkMode from "../../public/icons/dark_mode.svg";
+import Menu from "../../public/icons/menu.svg";
+import Close from "../../public/icons/close.svg";
 import useTheme from "@/hooks/useTheme";
+import { useState } from "react";
 
 export default function TopNav() {
   const { scrollY, isScrolled, handleModeClick } = useTheme();
+  const [showMenu, setShowMenu] = useState(false);
+
   const pathname = usePathname();
 
   return (
@@ -17,18 +22,18 @@ export default function TopNav() {
         !isScrolled && scrollY >= 100 ? "invisible" : "visible"
       } ${scrollY <= 100 ? "bg-none" : "bg-background-primary"} px-4 xl:px-0`}
     >
-      <div className="mx-auto flex h-full w-[1200px] items-center justify-between">
+      <div className="mx-auto flex h-full w-full max-w-[1200px] items-center justify-between">
         <div className="flex gap-32">
           <h1
             className={`text-2xl font-semibold ${
               scrollY <= 100 && pathname === "/"
-                ? "text-[white]"
+                ? "text-text-primary md:text-[white]"
                 : "text-text-primary"
             }`}
           >
             <Link href={"/"}>WAKCRAFT</Link>
           </h1>
-          <div className="flex items-center gap-20">
+          <div className="hidden items-center gap-20 md:flex">
             <h2
               className={`${
                 scrollY <= 100 && pathname === "/"
@@ -67,11 +72,11 @@ export default function TopNav() {
             </h2>
           </div>
         </div>
-        <div className="flex items-center">
+        <div className="flex items-center gap-4">
           <span
             className={`flex ${
               scrollY <= 100 && pathname === "/"
-                ? "fill-[#ccc] hover:fill-[white]"
+                ? " fill-text-primary md:fill-[#ccc] md:hover:fill-[white]"
                 : "fill-text-primary hover:fill-text-primary"
             } hover:cursor-pointer [&>*:first-child]:dark:hidden [&>*:last-child]:hidden [&>*:last-child]:dark:block`}
             onClick={handleModeClick}
@@ -79,8 +84,79 @@ export default function TopNav() {
             <LightMode />
             <DarkMode />
           </span>
+          <span
+            className="md:hidden [&>svg]:z-10 [&>svg]:h-7 [&>svg]:w-7 [&>svg]:fill-text-primary [&>svg]:hover:scale-105"
+            onClick={() => {
+              setShowMenu((prev) => !prev);
+
+              if (showMenu) {
+                allowScroll();
+              } else {
+                preventScroll();
+              }
+            }}
+          >
+            {showMenu ? <Close /> : <Menu />}
+          </span>
         </div>
       </div>
+      {showMenu && (
+        <div className="fixed bottom-0 right-0 h-[calc(100vh-80px)] w-full bg-[rgba(0,0,0,0.8)] md:hidden">
+          <ul
+            className={`flex flex-col gap-2 rounded-b-3xl bg-background-primary pb-2 text-center text-xl text-text-primary [&>a]:w-full [&>a]:p-4 `}
+          >
+            <Link
+              href={"/architect"}
+              onClick={() => {
+                setShowMenu(false);
+                allowScroll();
+              }}
+            >
+              <li>건축가</li>
+            </Link>
+            <Link
+              href={"/noobprohacker"}
+              onClick={() => {
+                setShowMenu(false);
+                allowScroll();
+              }}
+            >
+              <li>눕프로해커</li>
+            </Link>
+            <Link
+              href={"/content"}
+              onClick={() => {
+                setShowMenu(false);
+                allowScroll();
+              }}
+            >
+              <li>컨텐츠</li>
+            </Link>
+            <Link
+              href={"/game"}
+              onClick={() => {
+                setShowMenu(false);
+                allowScroll();
+              }}
+            >
+              <li>게임</li>
+            </Link>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
+
+const preventScroll = () => {
+  document.body.style.position = "fixed";
+  document.body.style.width = "100%";
+  document.body.style.overflowY = "scroll";
+};
+
+const allowScroll = () => {
+  document.body.style.position = "";
+  document.body.style.width = "";
+  document.body.style.top = "";
+  document.body.style.overflowY = "";
+};
