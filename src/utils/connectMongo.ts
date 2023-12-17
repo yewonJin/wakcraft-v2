1fcea19f8c3a956a4930f7e5dff676ecefd0d080
yewonJin <jyw966@naver.com>
2023-12-17 17:42:47 +0900

    [UPDATE] MongoDB Atlas 서버 작동 안하면 EC2 MongoDB 서버에 연결하기

--- src/utils/connectMongo.ts
+++ src/utils/connectMongo.ts
@@ -1,5 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectMongo = async () => mongoose.connect(process.env.MONGO_URI as string);
+const connectMongo = async () =>
+  mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(() => console.log("DB Connection Successfull"))
+    .catch((err) => {
+      console.log(err);
+
+      mongoose.connect(process.env.EC2_MONGO_URI as string, {
+        dbName: "wakcraft",
+      });
+    });
 
 export default connectMongo;
