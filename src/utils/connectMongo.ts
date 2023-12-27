33a6b98ab9136b60f810a732c324323a8459fc2f
yewonJin <jyw966@naver.com>
2023-12-28 00:39:12 +0900

    [UPDATE] DB 서버 EC2로 변경

--- src/utils/connectMongo.ts
+++ src/utils/connectMongo.ts
@@ -1,15 +1,8 @@
 import mongoose from "mongoose";
 
 const connectMongo = async () =>
-  mongoose
-    .connect(process.env.MONGO_URI as string)
-    .then(() => console.log("DB Connection Successfull"))
-    .catch((err) => {
-      console.log(err);
-
-      mongoose.connect(process.env.EC2_MONGO_URI as string, {
-        dbName: "wakcraft",
-      });
-    });
+  mongoose.connect(process.env.EC2_MONGO_URI as string, {
+    dbName: "wakcraft",
+  });
 
 export default connectMongo;
