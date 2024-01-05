f1164451807c533e3fd5d78a882119e5863f46d5
yewonJin <jyw966@naver.com>
2024-01-05 17:19:56 +0900

    [UPDATE] 메모이제이션 적용 해제

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback, useMemo, useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 import { Architect } from "@/domains/architect";
 import { fuzzySearch, fuzzySearchRegExp } from "@/utils/lib";
@@ -14,72 +14,60 @@ const useSearch = (architects: Architect[]) => {
     setInput("");
   };
 
-  const generateIndexOfMatches = useCallback(
-    (id: string) => {
-      const inputCharArr = input.toLowerCase().split("");
+  const generateIndexOfMatches = (id: string) => {
+    const inputCharArr = input.toLowerCase().split("");
 
-      const indexArr: number[] = [];
+    const indexArr: number[] = [];
 
-      let i = 0;
+    let i = 0;
 
-      inputCharArr.forEach((inputChar) => {
-        if (inputChar.match(/[ㄱ-힣]/g)) {
-          const result = id.match(fuzzySearchRegExp(inputChar));
+    inputCharArr.forEach((inputChar) => {
+      if (inputChar.match(/[ㄱ-힣]/g)) {
+        const result = id.match(fuzzySearchRegExp(inputChar));
 
-          if (!result) return;
+        if (!result) return;
 
-          indexArr.push(result.index as number);
-        }
+        indexArr.push(result.index as number);
+      }
 
-        i = id.toLowerCase().indexOf(inputChar, i);
-        indexArr.push(i++);
-      });
+      i = id.toLowerCase().indexOf(inputChar, i);
+      indexArr.push(i++);
+    });
 
-      return indexArr;
-    },
-    [input],
-  );
+    return indexArr;
+  };
 
-  const filterByFuzzySearch = useCallback(
-    (arr: Architect[]) => {
-      return arr.filter(
-        (item) =>
-          fuzzySearch(item.minecraft_id.toLowerCase(), input.toLowerCase()) ||
-          fuzzySearch(item.wakzoo_id.toLowerCase(), input.toLowerCase()),
-      );
-    },
-    [input],
-  );
+  const filterByFuzzySearch = (arr: Architect[]) => {
+    return arr.filter(
+      (item) =>
+        fuzzySearch(item.minecraft_id.toLowerCase(), input.toLowerCase()) ||
+        fuzzySearch(item.wakzoo_id.toLowerCase(), input.toLowerCase()),
+    );
+  };
 
-  const highlighting = useCallback(
-    (arr: Architect[]) => {
-      return arr
-        .map((architect) => ({
-          ...architect,
-          minecraftIdIndexArr: generateIndexOfMatches(architect.minecraft_id),
-          wakzooIdIndexArr: generateIndexOfMatches(architect.wakzoo_id),
-        }))
-        .sort((a, b) => {
-          if (a.minecraftIdIndexArr.includes(-1)) {
-            return (
-              parseInt(a.wakzooIdIndexArr.join("")) -
-              parseInt(b.minecraftIdIndexArr.join(""))
-            );
-          }
-
+  const highlighting = (arr: Architect[]) => {
+    return arr
+      .map((architect) => ({
+        ...architect,
+        minecraftIdIndexArr: generateIndexOfMatches(architect.minecraft_id),
+        wakzooIdIndexArr: generateIndexOfMatches(architect.wakzoo_id),
+      }))
+      .sort((a, b) => {
+        if (a.minecraftIdIndexArr.includes(-1)) {
           return (
-            parseInt(a.minecraftIdIndexArr.join("")) -
+            parseInt(a.wakzooIdIndexArr.join("")) -
             parseInt(b.minecraftIdIndexArr.join(""))
           );
-        });
-    },
-    [input],
-  );
+        }
 
-  const highlightedArchitects = useMemo(
-    () => highlighting(filterByFuzzySearch(architects)),
-    [input, architects],
-  );
+        return (
+          parseInt(a.minecraftIdIndexArr.join("")) -
+          parseInt(b.minecraftIdIndexArr.join(""))
+        );
+      });
+  };
+
+  const highlightedArchitects = highlighting(filterByFuzzySearch(architects));
 
   return {
     input,
