5dd335fffc297a2dd3dc998aad16e567f02afd60
yewonJin <jyw966@naver.com>
2024-01-06 17:08:42 +0900

    [UPDATE] 공용 컴포넌트 추가

---
+++ src/components/common/Select/Select.tsx
@@ -0,0 +1,32 @@
+import { ChangeEvent } from "react";
+
+type Props = {
+  value: string;
+  options: string[];
+  handleSelectChange: (e: ChangeEvent<HTMLSelectElement>) => void;
+  width?: string;
+  height?: string;
+};
+
+export default function Select(props: Props) {
+  const { value, options, handleSelectChange, width, height } = props;
+
+  return (
+    <select
+      value={value}
+      onChange={handleSelectChange}
+      className="rounded-md border-2 border-background-tertiary bg-background-primary pl-2 text-text-secondary outline-none"
+      style={{ width: width, height: height }}
+    >
+      {options.map((option) => (
+        <option
+          className="bg-background-primary hover:cursor-pointer"
+          value={option}
+          key={option}
+        >
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+}
