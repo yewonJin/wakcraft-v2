18abb76afd19b40972cc207739aa5c4d981be5b8
yewonJin <jyw966@naver.com>
2024-01-13 17:19:11 +0900

    [REFACTORING] 공용 컴포넌트 사용

--- src/components/Home/Calendar/CalendarTools.tsx
+++ src/components/Home/Calendar/CalendarTools.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/common/Button/Button";
+
 type Props = {
   setToday: () => void;
   setDateToStart: () => void;
@@ -7,19 +9,19 @@ export default function CalendarTools(props: Props) {
   const { setToday, setDateToStart } = props;
 
   return (
-    <div className="flex gap-2">
-      <span
-        className=" rounded-xl bg-background-secondary p-2 px-3 text-sm text-text-secondary hover:cursor-pointer hover:bg-background-tertiary"
-        onClick={() => setToday()}
-      >
-        오늘
-      </span>
-      <span
-        className=" rounded-xl bg-background-secondary p-2 px-3 text-sm text-text-secondary hover:cursor-pointer hover:bg-background-tertiary"
-        onClick={() => setDateToStart()}
-      >
-        처음
-      </span>
+    <div className="flex gap-2 [&>button]:text-sm">
+      <Button
+        value="오늘"
+        handleButtonClick={setToday}
+        padding="8px 12px"
+        height="36px"
+      />
+      <Button
+        value="처음"
+        handleButtonClick={setDateToStart}
+        padding="8px 12px"
+        height="36px"
+      />
     </div>
   );
 }
