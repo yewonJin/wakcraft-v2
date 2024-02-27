0a7e3b0a4b86868a7193a768a07a75595a42639d
yewonJin <jyw966@naver.com>
2024-02-27 16:37:17 +0900

    [UPDATE] 배치고사 순서로 정렬

--- src/components/Content/PlacementTest/ParticipantList.tsx
+++ src/components/Content/PlacementTest/ParticipantList.tsx
@@ -11,15 +11,17 @@ export default function ParticipantList(props: Props) {
 
   return (
     <div className="mt-16 grid grid-cols-1 gap-6 gap-y-12 md:grid-cols-2 2xl:w-[1400px] 2xl:grid-cols-3">
-      {participants.map((participant) => (
-        <div key={participant.minecraft_id} className="relative">
-          <ImageBox
-            imageUrl={participant.image_url}
-            minecraft_id={participant.minecraft_id}
-          />
-          <ArchitectureInfo placementResult={participant.placement_result} />
-        </div>
-      ))}
+      {participants
+        .sort((a, b) => a.order - b.order)
+        .map((participant) => (
+          <div key={participant.minecraft_id} className="relative">
+            <ImageBox
+              imageUrl={participant.image_url}
+              minecraft_id={participant.minecraft_id}
+            />
+            <ArchitectureInfo placementResult={participant.placement_result} />
+          </div>
+        ))}
     </div>
   );
 }
