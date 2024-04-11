fcf7aec0962f8c3af2ecdd42fe10bcccbdfc6413
yewonJin <jyw966@naver.com>
2024-04-12 03:46:29 +0900

    [공지] 이미지 서버 교체 완료

--- src/components/Home/Main/Main.tsx
+++ src/components/Home/Main/Main.tsx
@@ -17,9 +17,11 @@ export default function Main(props: Props) {
         " z-10 mx-auto flex h-auto w-full max-w-[1200px] flex-col justify-center px-4 pt-24 md:h-[100vh] md:pt-0 xl:px-0"
       }
     >
-      <h2 className="text-center text-3xl text-[white]">
-        현재 이미지 서버 교체중..
-      </h2>
+      <MainTitle
+        episode={noobprohacker.contentInfo.episode}
+        subject={noobprohacker.contentInfo.main_subject}
+      />
+      <MainInfo lines={noobprohacker.lineInfo} />
     </div>
   );
 }
